2cb5b31f7fc9b034276d0f4d024e509b135b36cb
Jayson <leroy.farming@gmail.com>
2025-09-29 15:53:31 +0200

    Update : API Gestion

--- js/anime.js
+++ js/anime.js
@@ -1,4 +1,6 @@
 let key = prompt("Déposer votre clé d'API ici pour faire des recherches");
+let search = document.getElementById('search');
+let getSearch = search.getAttribute('recherche');
 
 const data = null;
 
@@ -14,17 +16,27 @@ xhr.addEventListener('readystatechange', function () {
 xhr.setRequestHeader('x-rapidapi-key', key);
 xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
 
+function searchAnime() {
+}
+
 //Obtenir tout
-xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc');
+function getAll() {
+    xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc');
+}
 
 //Obtenir les genres
-xhr.open('GET', 'https://anime-db.p.rapidapi.com/genre');
+function getGenre() {
+    xhr.open('GET', 'https://anime-db.p.rapidapi.com/genre');
+}
 
 //Obtenir un anime en le classant
-xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime/by-ranking/1');
+function getAnimeClassement() {
+    xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime/by-ranking/1');
+}
 
 //Obtenir par identifiant
-xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime/by-id/1');
-
+function getID() {
+    xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime/by-id/1');
+}
 
 xhr.send(data);
