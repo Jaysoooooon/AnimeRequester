f9f9de756c56f6513c05f727302dcd10acca4270
DarkNatha008 <nathanael.heyberger@gmail.com>
2025-09-30 14:44:28 +0200

    feat: génération dynamique de la structure des cartes

--- js/anime.js
+++ js/anime.js
@@ -1,28 +1,39 @@
+function générez_résultat(){
+    if ("content" in document.createElement("template")) {
+        let template=document.getElementById("template_anime_card")
+        let all=document.getElementById("all")
+        for(let i = 0; i<10; i++){
+            let clone = document.importNode(template.content, true);
+            all.appendChild(clone);
+            let synopsis=document.getElementById("synopsis")
+            synopsis.textContent="syn"+i
+            synopsis.id=synopsis.id+"_"+i
+            let anime_title=document.getElementById("anime_title");
+            anime_title.textContent="title"+i
+            anime_title.id=anime_title.id+"_"+i
+            let image_anime=document.getElementById("image_anime");
+            image_anime.id=image_anime.id+"_"+i;
+            let catégories=document.getElementById("catégories");
+            catégories.id=catégories.id+"_"+i
+            let classement=document.getElementById("classement");
+            classement.id=classement.id+"_"+i;
+            let nb_episodes=document.getElementById("nb_episodes");
+            classement.id=classement.id+"_"+i;
+
+
+
+
+        }   
+    }
+}
+générez_résultat()
+
 let envoyer = document.getElementById('envoyer')
 let effacer = document.getElementById('effacer')
 let rechercher = document.getElementById('rechercher')
-if ("content" in document.createElement("template")) {
-    let template=document.getElementById("template_anime_card")
-    let all=document.getElementById("all")
-    for(let i = 0; i<10; i++){
-        let clone = document.importNode(template.content, true);
-        all.appendChild(clone);
-        let synopsis=document.getElementById("synopsis")
-        synopsis.textContent="syn"+i
-        synopsis.id=synopsis.id+"_"+i
-
-
-
-    }
-    
-    
-}
-
-
 
 /* code pour effacer la recherche */
 
 effacer.addEventListener('click', ()=>{
-    
     HTMLFormElement.reset();
 })
