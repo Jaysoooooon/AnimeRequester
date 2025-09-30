28e444c0c8f06d66d40c559367b9dfc1e653f97d
DarkNatha008 <nathanael.heyberger@gmail.com>
2025-09-30 14:26:13 +0200

    feat: dynamisation

--- js/anime.js
+++ js/anime.js
@@ -1,14 +1,16 @@
 if ("content" in document.createElement("template")) {
     let template=document.getElementById("template_anime_card")
     let all=document.getElementById("all")
-    let clone = document.importNode(template.content, true);
-    all.appendChild(clone);
-    let synopsis=document.getElementById("synopsis")
-    synopsis.textContent="syn1"
-    synopsis.id="2"
-    clone = document.importNode(template.content, true);
-    all.appendChild(clone);
-    synopsis=document.getElementById("synopsis")
-    synopsis.textContent="syn2"
+    for(let i = 0; i<10; i++){
+        let clone = document.importNode(template.content, true);
+        all.appendChild(clone);
+        let synopsis=document.getElementById("synopsis")
+        synopsis.textContent="syn"+i
+        synopsis.id=synopsis.id+"_"+i
+
+
+
+    }
+    
     
 }
