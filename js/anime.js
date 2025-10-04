1fcab582a1dcd8cf90222d81cc8e1cf3b5ab9560
Nathanael Heyberger <nathanael.heyberger@etu.unicaen.fr>
2025-10-04 22:41:21 +0200

    fix: correction d'erreurs issues du merge

--- js/anime.js
+++ js/anime.js
@@ -61,21 +61,6 @@ choix.addEventListener("change", ()=>{
 })
 
 
-if ("content" in document.createElement("template")) {
-    let template=document.getElementById("template_anime_card")
-    let all=document.getElementById("all")
-    let clone = document.importNode(template.content, true);
-    all.appendChild(clone);
-    let synopsis=document.getElementById("synopsis")
-    synopsis.textContent="syn1"
-    synopsis.id="2"
-    clone = document.importNode(template.content, true);
-    all.appendChild(clone);
-    synopsis=document.getElementById("synopsis")
-    synopsis.textContent="syn2"
-    
-}
-  
 
 /* code pour effacer la recherche */
 
